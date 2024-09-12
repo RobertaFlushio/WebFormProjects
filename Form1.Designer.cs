1f519f36ba04d10af486f45a77542f237952a712
Roberta Rie Flushio Ohata <160445345+RobertaFlushio@users.noreply.github.com>
2024-09-11 21:57:32 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace RadioECheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chckBxDesDesk = new System.Windows.Forms.CheckBox();
+            this.chckBxProbEst = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdBtnMundial = new System.Windows.Forms.RadioButton();
+            this.rdBtnMundiel = new System.Windows.Forms.RadioButton();
+            this.rdBtnMundiil = new System.Windows.Forms.RadioButton();
+            this.rdBtnMundiol = new System.Windows.Forms.RadioButton();
+            this.rdBtnMundiul = new System.Windows.Forms.RadioButton();
+            this.rdBtnSamsung = new System.Windows.Forms.RadioButton();
+            this.rdBtnLG = new System.Windows.Forms.RadioButton();
+            this.rdBtnEletrolux = new System.Windows.Forms.RadioButton();
+            this.rdBtnBrastemp = new System.Windows.Forms.RadioButton();
+            this.rdBtnMondial = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chckBxDesDesk
+            // 
+            this.chckBxDesDesk.AutoSize = true;
+            this.chckBxDesDesk.Location = new System.Drawing.Point(16, 38);
+            this.chckBxDesDesk.Margin = new System.Windows.Forms.Padding(2);
+            this.chckBxDesDesk.Name = "chckBxDesDesk";
+            this.chckBxDesDesk.Size = new System.Drawing.Size(181, 17);
+            this.chckBxDesDesk.TabIndex = 0;
+            this.chckBxDesDesk.Text = "Desenvolvimento para Desktop I";
+            this.chckBxDesDesk.UseVisualStyleBackColor = true;
+            // 
+            // chckBxProbEst
+            // 
+            this.chckBxProbEst.AutoSize = true;
+            this.chckBxProbEst.Location = new System.Drawing.Point(16, 61);
+            this.chckBxProbEst.Margin = new System.Windows.Forms.Padding(2);
+            this.chckBxProbEst.Name = "chckBxProbEst";
+            this.chckBxProbEst.Size = new System.Drawing.Size(158, 17);
+            this.chckBxProbEst.TabIndex = 1;
+            this.chckBxProbEst.Text = "Probabilidade e Estatística I";
+            this.chckBxProbEst.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quais disciplinas você escolhe passar nesse semestre?";
+            // 
+            // rdBtnMundial
+            // 
+            this.rdBtnMundial.AutoSize = true;
+            this.rdBtnMundial.Location = new System.Drawing.Point(18, 27);
+            this.rdBtnMundial.Name = "rdBtnMundial";
+            this.rdBtnMundial.Size = new System.Drawing.Size(62, 17);
+            this.rdBtnMundial.TabIndex = 4;
+            this.rdBtnMundial.TabStop = true;
+            this.rdBtnMundial.Text = "Mundial";
+            this.rdBtnMundial.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnMundiel
+            // 
+            this.rdBtnMundiel.AutoSize = true;
+            this.rdBtnMundiel.Location = new System.Drawing.Point(18, 50);
+            this.rdBtnMundiel.Name = "rdBtnMundiel";
+            this.rdBtnMundiel.Size = new System.Drawing.Size(62, 17);
+            this.rdBtnMundiel.TabIndex = 5;
+            this.rdBtnMundiel.TabStop = true;
+            this.rdBtnMundiel.Text = "Mundiel";
+            this.rdBtnMundiel.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnMundiil
+            // 
+            this.rdBtnMundiil.AutoSize = true;
+            this.rdBtnMundiil.Location = new System.Drawing.Point(18, 73);
+            this.rdBtnMundiil.Name = "rdBtnMundiil";
+            this.rdBtnMundiil.Size = new System.Drawing.Size(58, 17);
+            this.rdBtnMundiil.TabIndex = 6;
+            this.rdBtnMundiil.TabStop = true;
+            this.rdBtnMundiil.Text = "Mundiil";
+            this.rdBtnMundiil.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnMundiol
+            // 
+            this.rdBtnMundiol.AutoSize = true;
+            this.rdBtnMundiol.Location = new System.Drawing.Point(18, 96);
+            this.rdBtnMundiol.Name = "rdBtnMundiol";
+            this.rdBtnMundiol.Size = new System.Drawing.Size(62, 17);
+            this.rdBtnMundiol.TabIndex = 7;
+            this.rdBtnMundiol.TabStop = true;
+            this.rdBtnMundiol.Text = "Mundiol";
+            this.rdBtnMundiol.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnMundiul
+            // 
+            this.rdBtnMundiul.AutoSize = true;
+            this.rdBtnMundiul.Location = new System.Drawing.Point(18, 119);
+            this.rdBtnMundiul.Name = "rdBtnMundiul";
+            this.rdBtnMundiul.Size = new System.Drawing.Size(62, 17);
+            this.rdBtnMundiul.TabIndex = 8;
+            this.rdBtnMundiul.TabStop = true;
+            this.rdBtnMundiul.Text = "Mundiul";
+            this.rdBtnMundiul.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnSamsung
+            // 
+            this.rdBtnSamsung.AutoSize = true;
+            this.rdBtnSamsung.Location = new System.Drawing.Point(20, 119);
+            this.rdBtnSamsung.Name = "rdBtnSamsung";
+            this.rdBtnSamsung.Size = new System.Drawing.Size(69, 17);
+            this.rdBtnSamsung.TabIndex = 14;
+            this.rdBtnSamsung.TabStop = true;
+            this.rdBtnSamsung.Text = "Samsung";
+            this.rdBtnSamsung.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnLG
+            // 
+            this.rdBtnLG.AutoSize = true;
+            this.rdBtnLG.Location = new System.Drawing.Point(20, 96);
+            this.rdBtnLG.Name = "rdBtnLG";
+            this.rdBtnLG.Size = new System.Drawing.Size(39, 17);
+            this.rdBtnLG.TabIndex = 13;
+            this.rdBtnLG.TabStop = true;
+            this.rdBtnLG.Text = "LG";
+            this.rdBtnLG.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnEletrolux
+            // 
+            this.rdBtnEletrolux.AutoSize = true;
+            this.rdBtnEletrolux.Location = new System.Drawing.Point(20, 73);
+            this.rdBtnEletrolux.Name = "rdBtnEletrolux";
+            this.rdBtnEletrolux.Size = new System.Drawing.Size(65, 17);
+            this.rdBtnEletrolux.TabIndex = 12;
+            this.rdBtnEletrolux.TabStop = true;
+            this.rdBtnEletrolux.Text = "Eletrolux";
+            this.rdBtnEletrolux.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnBrastemp
+            // 
+            this.rdBtnBrastemp.AutoSize = true;
+            this.rdBtnBrastemp.Location = new System.Drawing.Point(20, 50);
+            this.rdBtnBrastemp.Name = "rdBtnBrastemp";
+            this.rdBtnBrastemp.Size = new System.Drawing.Size(69, 17);
+            this.rdBtnBrastemp.TabIndex = 11;
+            this.rdBtnBrastemp.TabStop = true;
+            this.rdBtnBrastemp.Text = "Brastemp";
+            this.rdBtnBrastemp.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnMondial
+            // 
+            this.rdBtnMondial.AutoSize = true;
+            this.rdBtnMondial.Location = new System.Drawing.Point(20, 27);
+            this.rdBtnMondial.Name = "rdBtnMondial";
+            this.rdBtnMondial.Size = new System.Drawing.Size(62, 17);
+            this.rdBtnMondial.TabIndex = 10;
+            this.rdBtnMondial.TabStop = true;
+            this.rdBtnMondial.Text = "Mondial";
+            this.rdBtnMondial.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdBtnMondial);
+            this.groupBox1.Controls.Add(this.rdBtnSamsung);
+            this.groupBox1.Controls.Add(this.rdBtnBrastemp);
+            this.groupBox1.Controls.Add(this.rdBtnLG);
+            this.groupBox1.Controls.Add(this.rdBtnEletrolux);
+            this.groupBox1.Location = new System.Drawing.Point(180, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(167, 156);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "O Palmeiras deseja muito?";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdBtnMundial);
+            this.groupBox2.Controls.Add(this.rdBtnMundiel);
+            this.groupBox2.Controls.Add(this.rdBtnMundiul);
+            this.groupBox2.Controls.Add(this.rdBtnMundiil);
+            this.groupBox2.Controls.Add(this.rdBtnMundiol);
+            this.groupBox2.Location = new System.Drawing.Point(16, 101);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(158, 156);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "O Palmeiras não tem?";
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Location = new System.Drawing.Point(353, 194);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(75, 23);
+            this.btnResultado.TabIndex = 17;
+            this.btnResultado.Text = "Resultado";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(353, 234);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 18;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 336);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chckBxProbEst);
+            this.Controls.Add(this.chckBxDesDesk);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chckBxDesDesk;
+        private System.Windows.Forms.CheckBox chckBxProbEst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdBtnMundial;
+        private System.Windows.Forms.RadioButton rdBtnMundiel;
+        private System.Windows.Forms.RadioButton rdBtnMundiil;
+        private System.Windows.Forms.RadioButton rdBtnMundiol;
+        private System.Windows.Forms.RadioButton rdBtnMundiul;
+        private System.Windows.Forms.RadioButton rdBtnSamsung;
+        private System.Windows.Forms.RadioButton rdBtnLG;
+        private System.Windows.Forms.RadioButton rdBtnEletrolux;
+        private System.Windows.Forms.RadioButton rdBtnBrastemp;
+        private System.Windows.Forms.RadioButton rdBtnMondial;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RadioECheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            int creditos = 0;
+            if (chckBxDesDesk.Checked)
+            {
+                creditos++;
+            }
+            if (chckBxProbEst.Checked)
+            {
+                creditos++;
+            }
+            int pontos = 0;
+            if (rdBtnMundial.Checked)
+            {
+                pontos++;
+            }
+            if (rdBtnMondial.Checked)
+            {
+                pontos++;
+            }
+            //verifica se foi escolhida alguma disciplina
+            if (creditos > 0)
+            {
+                if (pontos < creditos)
+                {
+                    DialogResult resultado = MessageBox.Show("Você não possui pontos suficientes. " +
+                        "Deseja continuar?",
+                        "Teste de Conhecimento", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+
+                    if (resultado == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+                    else if (resultado == DialogResult.Yes)
+                    {
+                        limparFornulario();
+                    }
+                    else
+                    {
+                        //clicou no x
+                        MessageBox.Show("voce fechou com o 'X'.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Parabéns, você conseguiu pontos suficientes");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Você deve escolher 1 ou 2 disciplinas!",
+                    "Teste de Conhecimento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void limparFornulario()
+        {
+            //linpar o form
+            chckBxDesDesk.Checked = false;
+            chckBxProbEst.Checked = false;
+            rdBtnMundial.Checked = false;
+            rdBtnMundiel.Checked = false;
+            rdBtnMundiil.Checked = false;
+            rdBtnMundiol.Checked = false;
+            rdBtnMundiul.Checked = false;
+
+            rdBtnMondial.Checked = false;
+            rdBtnSamsung.Checked = false;
+            rdBtnBrastemp.Checked = false;
+            rdBtnEletrolux.Checked = false;
+            rdBtnLG.Checked = false;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("Voce realmente deseja sair? ",
+                "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+
+            if (resultado == DialogResult.No)
+            {
+                Application.Exit();
+            }
+            else if (resultado == DialogResult.Yes)
+            {
+                limparFornulario();
+            }
+            else
+            {
+                //clicou no x
+                MessageBox.Show("voce fechou com o 'X'.");
+            }
+        }
+    }
+}
